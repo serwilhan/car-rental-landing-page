9b2f8a00cb6097af388fa51b65458342706401e1
serwilhan <serwilhan@gmail.com>
2022-03-17 09:26:52 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// function count() {
+//   const nums = [9, 2, 5, 6, 7, 8];
+
+//   for (let x of nums) {
+//     console.log(x);
+//   }
+// }
+
+// count();
+
+const nums = [9, 2, 5, 6, 7, 8];
+
+// const result = nums[2] + nums[3];
+
+// console.log(result);
